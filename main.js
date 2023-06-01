de8cecff562c71abc87656813dd46b5d7bf47e75
unknown <thevien1210@gmail.com>
2023-06-01 09:45:38 +0700

    change some teat

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const Koa = require("koa")
-const app = new Koa()
-const express = require('express')
-// const app = express()
-const cors = require('cors')
+const Koa = require("koa");
+const app = new Koa();
+const cors = require('koa-cors');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
-// app.use(cors())
-// app.get("/binance",(req, res)=>{
-//   res.send("Hello world2")
-// })
-// dotenv.config()
 
-// var con = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE
-// });
+dotenv.config();
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   con.query("SELECT * FROM agency", function (err, result, fields) {
-//     if (err) throw err;
-//     app.get("/binance2",(req, res)=>{
-//       res.send(result)
-//     })
-//     console.log(result);
-//   });
-// });
-app.use(async ctx =>{
-    ctx.body = 'Hello'
-})
+app.use(cors());
 
-const PORT = 5000
-app.listen(process.env.PORT || 5000,console.log("Run: "+PORT ))
+app.use(async (ctx, next) => {
+  if (ctx.path === "/binance") {
+    ctx.body = "Hello world2";
+  } else if (ctx.path === "/binance2") {
+    const con = mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
+    });
+
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM agency", function (err, result, fields) {
+        if (err) throw err;
+        ctx.body = result;
+        console.log(result);
+      });
+    });
+  } else {
+    ctx.body = 'Hello';
+  }
+
+  await next();
+});
+
+const PORT = 5000;
+app.listen(process.env.PORT || 5000, console.log("Run: " + PORT));
