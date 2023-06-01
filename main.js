566931b2496c8db3d4ad3e4abc3dc0a1195c518d
unknown <thevien1210@gmail.com>
2023-06-01 09:14:50 +0700

    change some text

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 const Koa = require("koa")
-const app = new Koa()
+const appKoa = new Koa()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+app.use(cors())
+app.get("/binance",(req, res)=>{
+  res.send("Hello world2")
+})
+dotenv.config()
 
-app.use(async ctx =>{
+var con = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM agency", function (err, result, fields) {
+    if (err) throw err;
+    app.get("/binance2",(req, res)=>{
+      res.send(result)
+    })
+    console.log(result);
+  });
+});
+appKoa.use(async ctx =>{
     ctx.body = 'Hello'
 })
 
