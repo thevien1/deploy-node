4dbb41b1bfb9adbd1ff1a7bc37ae94aa9bbe60e2
unknown <thevien1210@gmail.com>
2023-06-02 08:23:12 +0700

    name gi daao

--- main.js
+++ main.js
@@ -7,7 +7,36 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(cors());
+const crypto = require('crypto');
+const axios = require('axios');
 
+const getDepositHistory = () => {
+  const apiKey = 'ehZHLTqpe5AyBCOIR73wnXtf4g7RZ0uYHOtak47RYT4OYnkLQsdueKmdNMI6abIc';
+  const apiSecret = '8irmwfRrD7At6W91ny2rjIOqEVxDWUOJHPi0JiulE6oh0yg47AqXBo6bjQivYYih';
+
+  const timestamp = Date.now();
+  const queryString = `timestamp=${timestamp}`;
+  const signature = crypto.createHmac('sha256', apiSecret).update(queryString).digest('hex');
+
+  const headers = { 'X-MBX-APIKEY': apiKey };
+  const params = { timestamp, signature };
+
+  return axios.get('https://api.binance.com/sapi/v1/capital/deposit/hisrec', { headers, params })
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Yêu cầu không thành công:', error.response.status, error.response.data);
+      throw error;
+    });
+};
+getDepositHistory()
+  .then(depositHistory => {
+
+    console.log(depositHistory);
+    ctx.body = depositHistory
+  })
+  .catch(error => {
+    // Xử lý lỗi nếu cần
+  });
 app.use(async (ctx, next) => {
   if (ctx.path === "/binance") {
     ctx.body = "Hello world2";
@@ -34,6 +63,14 @@ app.use(async (ctx, next) => {
     console.log(queryResult);
   } else {
     ctx.body = 'Hello';
+    getDepositHistory()
+  .then(depositHistory => {
+    ctx.body = depositHistory
+    console.log(depositHistory);
+  })
+  .catch(error => {
+    // Xử lý lỗi nếu cần
+  });
   }
 
   await next();
@@ -43,33 +80,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
-const crypto = require('crypto');
-const axios = require('axios');
 
-const getDepositHistory = () => {
-  const apiKey = 'ehZHLTqpe5AyBCOIR73wnXtf4g7RZ0uYHOtak47RYT4OYnkLQsdueKmdNMI6abIc';
-  const apiSecret = '8irmwfRrD7At6W91ny2rjIOqEVxDWUOJHPi0JiulE6oh0yg47AqXBo6bjQivYYih';
 
-  const timestamp = Date.now();
-  const queryString = `timestamp=${timestamp}`;
-  const signature = crypto.createHmac('sha256', apiSecret).update(queryString).digest('hex');
 
-  const headers = { 'X-MBX-APIKEY': apiKey };
-  const params = { timestamp, signature };
-
-  return axios.get('https://api.binance.com/sapi/v1/capital/deposit/hisrec', { headers, params })
-    .then(response => response.data)
-    .catch(error => {
-      console.error('Yêu cầu không thành công:', error.response.status, error.response.data);
-      throw error;
-    });
-};
-
-getDepositHistory()
-  .then(depositHistory => {
-    ctx.body = depositHistory
-    console.log(depositHistory);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu cần
-  });
