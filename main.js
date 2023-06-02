cd42d28799e9eb8b7df5fbd615a30555b8c8e474
unknown <thevien1210@gmail.com>
2023-06-02 09:05:01 +0700

    name gi daao

--- main.js
+++ main.js
@@ -38,7 +38,8 @@ app.use(async (ctx, next) => {
       host: process.env.MYSQL_HOST,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQL_PORT,
     });
 
     const queryResult = await new Promise((resolve, reject) => {
